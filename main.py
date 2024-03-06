6b7ba619ac29a3438e510b3ad9e4d61e3f61716b
Alex Bryan <abryan9@uwyo.edu>
2024-03-06 09:46:49 -0700

    app frame built

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+# from tkinter import ttk
+
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+        
+        self.everythingy = tk.Entry()
+        self.everythingy.pack()
+        
+        self.contents = tk.StringVar()
+        self.contents.set("This is a variable")
+        self.everythingy["textvariable"] = self.contents
+        
+        self.everythingy.bind('<Key-Return>', self.print_contents)
+    
+    def print_contents(self, event):
+        print("The current entries are:",
+                self.contents.get())
+    
+
+root = tk.Tk()
+myapp = App(root)
+# frm = tk.Frame(myapp, padding=10)
+# frm.grid()
+# tk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# tk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+myapp.mainloop()
