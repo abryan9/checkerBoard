03056c99ccd14569e66afdf4d1b77c58b6d8a265
Alex Bryan <abryan9@uwyo.edu>
2024-03-14 10:43:12 -0600

    Data is being processed and formats for time check

--- main.py
+++ main.py
@@ -9,73 +9,73 @@ class CheckerBoard(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         
+        frame = tk.Frame(master)
+        
         self.zones = ["All Zones", "Zone 1", "Zone 2", "Zone 3", "Zone 4", "No Zone Selected"]
         self.zone = tk.StringVar()
-        self.selectedZoneList = []
         
-        self.scroll = tk.Scrollbar(self)
-        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
-        self.listRooms = tk.Listbox(self, selectmode=tk.SINGLE, yscrollcommand=self.scroll.set)
+        self.select_zone()
+        
+        tk.Label(frame, text='test').pack(side=tk.TOP, fill=tk.X, anchor=tk.N, expand=0)
+        
+        self.yscroll = tk.Scrollbar(frame)
+        self.yscroll.pack(side=tk.LEFT, anchor=tk.E, fill=tk.Y, pady=(0,25))
+        self.listRooms = tk.Listbox(frame, yscrollcommand=self.yscroll.set)
+        self.listRooms.pack(side=tk.TOP, anchor=tk.N, fill=tk.BOTH, expand=1, pady=(0,10))
         
         self.ABS_PATH = __file__.removesuffix('main.py')
         
         self.zone_config = zc.zone_config()
         
-        self.pack()
+        self.pack(fill='both', expand=1)
+        
+        tk.Button(frame, text='Quit', command=root.destroy).pack(side=tk.RIGHT, anchor=tk.E, padx=(1,0))
+        tk.Button(frame, text='Save').pack(side=tk.RIGHT, anchor=tk.E, padx=(1,0))
+        tk.Button(frame, text='Refresh').pack(side=tk.RIGHT, anchor=tk.E, padx=(1,0))
+        
+        frame.pack(fill='both', padx=10, pady=10, expand=1)
    
 
     def select_zone(self):
-        self.zone.set("No Zone Selected")
+        self.zone.set('No Zone Selected')
 
         dropdown = tk.OptionMenu(self, self.zone, *self.zones, command=self.show_rooms)
-        dropdown.pack()
+        dropdown.pack(side=tk.TOP, anchor=tk.N, expand=0)
         
         
         pass
 
 
     def show_clicked(self, event):
-        self.label.config(text=self.zone.get())
+        frame.label.config(text=self.zone.get())
         pass
+    
+    
+    def get_zone_list(self, zone):
         
-        
+        if zone[:2] != 'No':
+            if zone == 'All Zones':
+                zoneNum = 0
+            else:
+                zoneNum = int(zone[-1])
+            return self.zone_config.get_zone(zoneNum)
+        else:
+            return ['']
+    
+    
     def show_rooms(self, zone):
-        zoneDict = { 
-            "All Zones": self.populate_zone(0), 
-            "Zone 1": self.populate_zone(1),
-            "Zone 2": self.populate_zone(2),
-            "Zone 3": self.populate_zone(3),
-            "Zone 4": self.populate_zone(4),
-            "No Zone Selected": self.populate_zone(5),
-        }
         
-        print(zoneDict[zone])
-        self.listRooms.delete(0, self.listRooms.size())
-        self.listRooms.insert(0, *zoneDict[zone])
-        self.listRooms.pack(side=tk.LEFT, fill=tk.BOTH)
-        self.scroll.config(command=self.listRooms.yview)
+        self.listRooms.delete(0, tk.END)
+        self.listRooms.insert(0, *self.get_zone_list(zone))
+        self.listRooms.pack(side=tk.TOP, fill='both', expand=1)
+        self.yscroll.config(command=self.listRooms.yview)
         pass
-        
-    
-    def populate_zone(self, zoneNum):
-    
-        self.selectedZoneList = self.zone_config.get_zone(zoneNum)
-            
-        return self.selectedZoneList
-    
-    
-    
-    
+
 
 root = tk.Tk()
-root.title("CheckerBoard")
-root.geometry("300x300")
+root.title('CheckerBoard')
+root.geometry('500x300')
 
 checkerBoard = CheckerBoard(root)
-frm = ttk.Frame(checkerBoard, padding=10)
-frm.pack()
 
-checkerBoard.select_zone()
-
-ttk.Button(root, text="Quit", command=root.destroy).pack()
 checkerBoard.mainloop()
--- zoneConfig.py
+++ zoneConfig.py
@@ -1,69 +1,104 @@
 import pandas as pd
 import os
+from datetime import date, datetime, timedelta
+import calendar
 
 class zone_config():
 
+    ABS_PATH = __file__.removesuffix('zoneConfig.py')
+    
+    allBuildings = '''
+        SI STEM EERB AN BC HS GE ES EIC
+        EN AG EA ED HA HI BU
+        CR PS AS BS NAC RH HO
+        PA FA CB LS AB AC VA
+        '''
+
     def __init__(self):
-        self.load_zones = self.generate_config()
-        
-        self.ABS_PATH = __file__.removesuffix('zoneConfig.py')
+        self.load_zones = self.pull_zones()
 
 
     def get_zone(self, zoneNum):
         zoneList = [
-            [['AG', 0], ['EERB', 0], ['STEM', 0], ['SI', 0], ['GE', 0], ['PS', 0], ['BS', 0], ['AB', 0], ['AC', 0], ['AS', 0], ['CR', 0], ['CL', 0], ['BU', 0], ['ITC', 0], ['PA', 0], ['LS', 0], ['VA', 0], ['CB', 0], ['HA', 0], ['ED', 0], ['EA', 0], ['ESB', 0], ['EIC', 0], ['HS', 0], ['NA', 0], ['HI', 0],],
-            ['AG'],
-            ['EN'],
-            ['CR'],
-            ['LS'],
-            [],
+            self.allBuildings,
+            'SI STEM EERB AN BC HS GE ES EIC',
+            'EN AG EA ED HA HI BU',
+            'CR PS AS BS NAC RH HO',
+            'PA FA CB LS AB AC VA',
         ]
         
-        return zoneList[zoneNum]
+        roomList = []
+        for room in self.load_zones.keys():
+            if room.split(" ")[0] in zoneList[zoneNum]:
+                roomList.append([room, self.load_zones[room]])
+                self.check_availability(self.load_zones[room])
+
+        return roomList
 
     
     def pull_zones(self):
-        if os.path.isFile(self.ABS_PATH + 'zone.config'):
-            config = open(self.ABS_PATH + 'zone.config', 'r')
+        roomsDict = {}
+        
+        if os.path.exists(self.ABS_PATH + 'rooms.conf'):
+            config = open(self.ABS_PATH + 'rooms.conf', 'r')
             for line in config:
-                print(line)
+                splitList = line[:-2].split(': ')
+                roomsDict.update({str(splitList[0]): str(splitList[1][1:])[1:-2].split('], [')})
                 
             config.close()
-        
-        return 0
+            
+        else:
+            roomsDict = self.generate_config()
+    
+        return roomsDict
     
 
     def generate_config(self):
-        roomList = [
-            'SI', 'STEM', 'EERB', 'AN', 'BC', 'HS', 'GE', 'ES', 'EIC',
-            'EN', 'AG', 'EA', 'ED', 'HA', 'HI', 'BU',
-            'CR', 'PS', 'AS', 'BS', 'NAC', 'RH', 'HO',
-            'PA', 'FA', 'CB', 'LS', 'AB', 'AC', 'VA',
-        ]
         
         ABS_PATH = __file__.removesuffix('zoneConfig.py')
         data = (ABS_PATH + 'roomConfig.csv')
         loadedData = pd.read_csv(data, names=['Rooms', 'Times', 'Contact', 'Empty'], engine='pyarrow', header=None)
+        roomFile = open(ABS_PATH + 'rooms.conf', 'w')
         
         rows, columns = loadedData.shape
         
         roomDict = {}
-        currentRoom = ''
+        currentRoom = 'AB 103'
         tempList = []
         for row in range(rows):
             search = loadedData.loc[row, 'Rooms']
-            if type(search) == str and search.split(" ")[0] in roomList:
+            if type(search) == str and search.split(" ")[0] in self.allBuildings:
                 if currentRoom != search:
-                    if currentRoom != '':
-                        roomDict.update({search: tempList})
+                    roomDict.update({currentRoom: tempList})
+                    roomFile.write(currentRoom + ': ' + str(tempList) + '\n')
                     currentRoom = search
                     tempList = []
-            
+    
             elif type(search) == str and search[0].isdigit():
-                tempList.append(search)
-                
-            
+                toAdd = loadedData.loc[row, 'Times'].split(', ')
+                if len(toAdd) > 1:
+                    toAdd = toAdd[1].split(' ')[:-2]
+                    ' '.join(toAdd)
+                    tempList.append(toAdd)
+                else:
+                    tempList.append('')
+                    
+        roomDict.update({currentRoom: tempList})
+        roomFile.write(currentRoom + ': ' + str(tempList) + '\n')
         
-        print(roomDict)
+        roomFile.close()
         
         return roomDict
+        
+        
+    def check_availability(self, roomList):
+        now = datetime.now()
+        dayOfWeek = now.strftime('%A')
+        
+        if dayOfWeek == 'Thursday':
+            dayOfWeek = 'R'
+        else:
+            dayOfWeek = dayOfWeek[0]
+            
+        for room in roomList:
+            print(room)
