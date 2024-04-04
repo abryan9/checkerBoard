85853be827bf4bdb4f765358a32a64f7731bd853
Alex Bryan <abryan9@uwyo.edu>
2024-04-04 11:52:05 -0600

    Minimal viable product until LSM API release.

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Welcome to the all-in-one interface for Room Check management!
 ## Goals ##
 Users should be able to open the CheckerBoard application, select their zone, and see an up-to-date list of the rooms in their zone, when they were last checked/if they need checked again, and their availability.
 Accomplishing this is dependant upon having ready access to the current tracking systems (namely LSM and 25Live) and proper implementation of all dependencies.
+
+## Known Issues ##
+* Resizing the window is very clunky
+* No LSM integration
+* Room availability might have an off-by-one bug
--- main.py
+++ main.py
@@ -10,67 +10,139 @@ class CheckerBoard(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         
-        frame = tk.Frame(master)
+        self.frame = tk.Frame(master)
+        self.frame.pack(expand=1)
+        self.header = tk.Frame(self.frame)
+        self.boxes = tk.Frame(self.frame)
+        self.buttons = tk.Frame(self.frame)
         
-        self.zones = ["All Zones", "Zone 1", "Zone 2", "Zone 3", "Zone 4", "No Zone Selected"]
+        self.zones = ['All Zones', 'Zone 1', 'Zone 2', 'Zone 3', 'Zone 4', 'No Zone Selected']
         self.zone = tk.StringVar()
         self.zone.set('No Zone Selected')
         
         self.select_zone()
         self.zone_config = zc.zone_config()
         
+        self.colorPallete = {
+            'UW Gold': '#ffc425',
+            'UW Brown': '#492f24',
+            'Charcoal': '#575a62',
+            'Slate': '#79706c',
+            'Prairie Grass': '#7e7615',
+            'Spring Leaves': '#a2a42c',
+            'Canyon Rock': '#874917',
+            'Cowboy Boot': '#9a651e',
+            'Indian Paintbrush': '#77160c',
+            'Evening Sky': '#2a3e60',
+            'Cool Grey 3': '#cbcccd',
+            'Cool Grey 1': '#ebeced',
+            'Light Beige': '#d4cbc6',
+        }
+        
         currentTime = datetime.now()
         currentDelta = timedelta(hours=currentTime.hour, minutes=currentTime.minute, seconds=int(currentTime.second))
         self.checkedTime = tk.StringVar()
         self.checkedTime.set(f'Last Updated: {currentDelta}')
-        self.timeLabel = tk.Label(frame, textvariable=self.checkedTime)
-        self.timeLabel.configure(bg='#cbcccd')
+        self.timeLabel = tk.Label(self.header, textvariable=self.checkedTime)
+        # self.timeLabel.configure(bg='#cbcccd', bd=0)
         self.timeLabel.pack_propagate(False)
-        self.timeLabel.pack(side=tk.TOP, anchor=tk.W, expand=0)
+        self.timeLabel.pack(side=tk.LEFT, anchor=tk.E)
         
-        self.yscroll = tk.Scrollbar(frame)
-        self.yscroll.configure(bg='#cbcccd')
-        self.yscroll.pack(side=tk.LEFT, anchor=tk.W, fill=tk.BOTH, pady=(0,40))
-        self.listRooms = tk.Listbox(frame, height=int(master.winfo_height()/25), yscrollcommand=self.yscroll.set)
-        self.listRooms.configure(bg='#cbcccd')
-        self.listRooms.pack(side=tk.TOP, anchor=tk.N, fill=tk.BOTH, expand=1, pady=(0,10))
+        self.yscroll = tk.Scrollbar(self.boxes, orient='vertical', command=self.onVsb)
+        # self.yscroll.configure(bg='#cbcccd')
+        self.yscroll.pack(side=tk.LEFT, fill=tk.Y, expand=0)
         
-        tk.Button.configure(self, bg='#cbcccd')
-        tk.Button(frame, text='Quit', command=root.destroy).pack(side=tk.RIGHT, anchor=tk.S, padx=(1,0))
-        tk.Button(frame, text='Save').pack(side=tk.RIGHT, anchor=tk.S, padx=(1,0))
-        tk.Button(frame, text='Refresh', command=self.refresh).pack(side=tk.RIGHT, anchor=tk.E, padx=(1,0))
+        self.listRooms = tk.Listbox(self.boxes, width=9, height=10, yscrollcommand=self.onMouseWheel)
+        # self.listRooms.configure(height=root.winfo_height()-100)
+        self.listRooms.pack(side=tk.LEFT, expand=0)
         
+        self.listDates = tk.Listbox(self.boxes, width=12, height=10, yscrollcommand=self.onMouseWheel)
+        self.listDates.pack(side=tk.LEFT, expand=0)
+        
+        self.listNeedsChecked = tk.Listbox(self.boxes, width=4, height=10, yscrollcommand=self.onMouseWheel)
+        self.listNeedsChecked.pack(side=tk.LEFT, expand=0)
+
+        self.listAvailable = tk.Listbox(self.boxes, width=25, height=10, yscrollcommand=self.onMouseWheel)
+        self.listAvailable.pack(side=tk.LEFT, expand=0)
+
+        self.listComments = tk.Listbox(self.boxes, height=10, yscrollcommand=self.yscroll.set)
+        self.listComments.pack(side=tk.LEFT, anchor=tk.W, fill=tk.X, expand=1)
+
+        self.listRooms.bind('<MouseWheel>', self.onMouseWheel)
+        self.listDates.bind('<MouseWheel>', self.onMouseWheel)
+        self.listNeedsChecked.bind('<MouseWheel>', self.onMouseWheel)
+        self.listAvailable.bind('<MouseWheel>', self.onMouseWheel)
+        self.listComments.bind('<MouseWheel>', self.onMouseWheel)
+        
+        # tk.Button.configure(self.buttons, bg='#cbcccd')
+        tk.Button(self.buttons, text='Quit', command=root.destroy).pack(side=tk.RIGHT, anchor=tk.S)
+        tk.Button(self.buttons, text='Save').pack(side=tk.RIGHT, anchor=tk.S)
+        tk.Button(self.buttons, text='Refresh', command=self.refresh).pack(side=tk.RIGHT, anchor=tk.S)
         self.ABS_PATH = __file__.removesuffix('main.py')
         
-        self.pack(fill=tk.BOTH, expand=1)
+        self.header.pack(side=tk.TOP, fill=tk.X, expand=1, pady=4, padx=4)
+        self.boxes.pack(side=tk.TOP, fill=tk.X, expand=0, padx=4)
+        self.buttons.pack(side=tk.TOP, fill=tk.X, expand=1, pady=4, padx=4)
         
-        frame.pack(side=tk.TOP, fill=tk.BOTH, padx=10, pady=10, expand=1)
+        self.frame.pack(fill=tk.BOTH, expand=1)
+        
+        
         
         return None
     
     
-    def set_list_height(self, passedHeight):
-        # print(passedHeight)
-        self.listRooms.config(height=int(passedHeight/25.5))
+    def onVsb(self, *args):
+        self.listRooms.yview(*args)
+        self.listDates.yview(*args)
+        self.listNeedsChecked.yview(*args)
+        self.listAvailable.yview(*args)
+        self.listComments.yview(*args)
+    
+    
+    def onMouseWheel(self, *args):
+        try:
+            convertedArgs = list(float(arg) for arg in args)
+            self.yscroll.set(convertedArgs[0], convertedArgs[-1])
+            self.listRooms.yview('moveto', args[0])
+            self.listDates.yview('moveto', args[0])
+            self.listNeedsChecked.yview('moveto', args[0])
+            self.listAvailable.yview('moveto', args[0])
+            self.listComments.yview('moveto', args[0])
+        except:
+            pass
+    
+    
+    def set_list_size(self, newHeight, newWidth):
+        offset=6
+        self.listRooms.configure(height=int(newHeight/19.04)-offset)
+        self.listDates.configure(height=int(newHeight/19.04)-offset)
+        self.listNeedsChecked.configure(height=int(newHeight/19.04)-offset)
+        self.listAvailable.configure(height=int(newHeight/19.04)-offset)
+        self.listComments.configure(height=int(newHeight/19.04)-offset, width=int(newWidth/19.04))
         return 0
-   
+
 
     def select_zone(self):
         self.zone.set('No Zone Selected')
 
-        dropdown = tk.OptionMenu(self, self.zone, *self.zones, command=self.show_rooms)
-        dropdown.configure(bg='#cbcccd')
+        dropdown = tk.OptionMenu(self.header, self.zone, *self.zones, command=self.show_rooms)
+        # dropdown.configure(bg='#cbcccd')
         dropdown.pack_propagate(False)
-        dropdown.pack(side=tk.TOP, anchor=tk.N, expand=0)
+        dropdown.pack(side=tk.TOP)
         return 0
 
 
     def refresh(self):
-        self.get_zone_list(str(self.zone))
+        self.get_zone_dict(str(self.zone))
         return 0
     
     
-    def get_zone_list(self, zone='No Zone Selected'):
+    def clear_tree(self):
+        for item in self.listTree.get_children():
+            self.listTree.delete(item)
+    
+    
+    def get_zone_dict(self, zone='No Zone Selected'):
     
         currentTime = datetime.now()
         currentDelta = timedelta(hours=currentTime.hour, minutes=currentTime.minute, seconds=int(currentTime.second))
@@ -88,37 +160,55 @@ class CheckerBoard(tk.Frame):
     
     def show_rooms(self, zone):
     
-        rawZoneList = self.get_zone_list(zone)
+        rawZoneDict = self.get_zone_dict(zone)
         
         self.listRooms.delete(0, tk.END)
-        self.listRooms.insert(0, *rawZoneList)
-        self.listRooms.config(bg='#cbcccd')
-        self.listRooms.pack(side=tk.TOP, fill=tk.X, anchor=tk.N, expand=1)
-        self.yscroll.config(bg='#cbcccd', command=self.listRooms.yview)
+        self.listDates.delete(0, tk.END)
+        self.listNeedsChecked.delete(0, tk.END)
+        self.listAvailable.delete(0, tk.END)
+        self.listComments.delete(0, tk.END)
         
-        for item in range(len(rawZoneList)):
-            if 'UNAVAILABLE' in str(self.listRooms.get(item)):
-                self.listRooms.itemconfig(item,{'bg':'#77160c', 'fg':'#ebeced'})
+        self.yscroll.config(command=self.listRooms.yview)
+        
+        for item in rawZoneDict.keys():
+            self.listRooms.insert(tk.END, item)
+            self.listDates.insert(tk.END, rawZoneDict[item]['Last Checked'])
+            
+            self.listNeedsChecked.insert(tk.END, rawZoneDict[item]['Needs Checked'])
+            if rawZoneDict[item]['Needs Checked'] == 'YES':
+                self.listNeedsChecked.itemconfig(tk.END,{'bg':self.colorPallete['UW Gold']})
             else:
-                self.listRooms.itemconfig(item,{'bg':'#525a62', 'fg':'#ebeced'})
+                self.listNeedsChecked.itemconfig(tk.END,{'bg':self.colorPallete['UW Brown'], 'fg':self.colorPallete['Cool Grey 1']})
+        
+            self.listAvailable.insert(tk.END, rawZoneDict[item]['Available'])
+            if 'UNAVAILABLE' not in rawZoneDict[item]['Available']:
+                self.listAvailable.itemconfig(tk.END,{'bg':self.colorPallete['Evening Sky'], 'fg':self.colorPallete['Cool Grey 1']})
+            else:
+                self.listAvailable.itemconfig(tk.END,{'bg':self.colorPallete['Indian Paintbrush'], 'fg':self.colorPallete['Cool Grey 1']})
+            
+            self.listComments.insert(tk.END, rawZoneDict[item]['Comments'])
+            
+        self.listRooms.pack(side=tk.LEFT, expand=0)
+        self.listDates.pack(side=tk.LEFT, expand=0)
+        self.listNeedsChecked.pack(side=tk.LEFT, expand=0)
+        self.listAvailable.pack(side=tk.LEFT, expand=0)
+        self.listComments.pack(side=tk.LEFT, fill=tk.X, expand=1)
         
         return 0
 
 
 root = tk.Tk()
-root.title('CheckerBoard')
+root.title('UWIT Checkerboard')
 root.geometry('750x630')
-root.option_add('*Font', 'consolas 12')
+root.option_add('*Font', 'courier 12')
 root.config(bg='#cbcccd')
 root.update_idletasks()
 checkerBoard = CheckerBoard(root)
 
 def onResize(event):
-    root.config(width=event.width, height=event.height)
-    checkerBoard.set_list_height(root.winfo_height())
+    root.config(height=event.height, width=event.width)
+    checkerBoard.set_list_size(root.winfo_height(), root.winfo_height())
     
 root.bind('<Configure>', onResize)
-
-# checkerBoard.bind('<Configure>', onResize)
 
 checkerBoard.mainloop()
